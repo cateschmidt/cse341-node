58c11f652296827594cb5f92010e20a4ca776a69
cateschmidt <c8schmidt@gmail.com>
2023-05-07 00:09:53 -0400

    initial commit

--- routes/contacts.js
+++ routes/contacts.js
@@ -6,7 +6,14 @@ const router = express.Router();
 
 // GET /feed/posts
 router.get('/', contactsController.getAll);
+
 router.get('/:id', contactsController.getSingle);
+
+router.post('/', contactsController.createContact);
+
+router.put('/:id', contactsController.updateContact);
+
+router.delete('/:id', contactsController.deleteContact);
 
 // localhost:8080/professional/
 module.exports = router;
