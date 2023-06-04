f143beee86344180d16e3fdbf2fd398f24017472
cateschmidt <c8schmidt@gmail.com>
2023-06-04 01:28:59 -0400

    comment out parts validation - fix next week

--- personalproject/utilities/validator.js
+++ personalproject/utilities/validator.js
@@ -28,27 +28,27 @@ const boatsValidate = (req, res, next) => {
 
 module.exports = {boatsRules, boatsValidate,};
 
-// Parts Validation/error handling
+// // Parts Validation/error handling
 
-const partsRules = () => {
-    return [
-        body("name").trim().escape().isLength({min:1}).withMessage("Error, please enter part name"),
-        body("quantity").trim().escape().isLength({min:1}).withMessage("Error, please enter a quantity"),
-        body("system").trim().escape().isLength({min:1}).withMessage("Error, please enter a valid system"),
-        body("upc").trim().escape().isLength({min:1}).withMessage("Error, please enter a upc")
-    ]
-}
+// const partsRules = () => {
+//     return [
+//         body("name").trim().escape().isLength({min:1}).withMessage("Error, please enter part name"),
+//         body("quantity").trim().escape().isLength({min:1}).withMessage("Error, please enter a quantity"),
+//         body("system").trim().escape().isLength({min:1}).withMessage("Error, please enter a valid system"),
+//         body("upc").trim().escape().isLength({min:1}).withMessage("Error, please enter a upc")
+//     ]
+// }
 
-const partsValidate = (req, res, next) => {
-    let errors = [];
-    errors = validationResult(req);
-    if (errors.isEmpty()) {
-        next();
-        return
-    }
-    else {
-        res.json(errors);
-    }
-}
+// const partsValidate = (req, res, next) => {
+//     let errors = [];
+//     errors = validationResult(req);
+//     if (errors.isEmpty()) {
+//         next();
+//         return
+//     }
+//     else {
+//         res.json(errors);
+//     }
+// }
 
-module.exports = {partsRules, partsValidate,};
+// module.exports = {partsRules, partsValidate,};
